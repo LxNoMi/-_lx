c682caa4fb9c36dcd0c2151fe1a15ca82dc170d6
liuxun <1541233570@qq.com>
2019-04-14 18:13:53 +0800

    first commit

---
+++ mainwindow.cpp
@@ -0,0 +1,87 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include<QListWidget>
+#include <QListWidgetItem>
+#include<QStringList>
+#include<QMessageBox>
+#include<QDir>
+#include<QDebug>
+#include<QFileDialog>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    ui->listWidget->setViewMode(QListView::IconMode);
+    QObject::connect(ui->addCityButton,SIGNAL(clicked()),this,SLOT(addCitySlot()));
+    QObject::connect(ui->cityNameEdit,SIGNAL(returnPressed()),this,SLOT(addCitySlot()));
+    QObject::connect(ui->delCityButton,SIGNAL(clicked()),this,SLOT(delCitySlot()));
+    QObject::connect(ui->delAllButton,SIGNAL(clicked()),this,SLOT(delAllSlot()));
+    QObject::connect(ui->showAllButton,SIGNAL(clicked()),this,SLOT(showFileSlot()));
+    QObject::connect(ui->listWidget,SIGNAL(itemClicked(QListWidgetItem *)),this,SLOT(singleClickedSlot(QListWidgetItem *)));
+    QObject::connect(ui->listWidget,SIGNAL(itemDoubleClicked(QListWidgetItem *)),this,SLOT(doubleClickedSlot(QListWidgetItem *)));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+//添加城市
+void MainWindow::addCitySlot()
+{
+    QString cityName = ui->cityNameEdit->text().trimmed();
+    QListWidgetItem *item=new QListWidgetItem;
+    item->setText(cityName);
+    ui->listWidget->addItem(item);
+    ui->cityNameEdit->clear();
+}
+
+//删除一个选中城市
+void MainWindow::delCitySlot()
+{
+    QListWidgetItem *item = ui->listWidget->takeItem(ui->listWidget->currentRow());
+    delete item;
+}
+
+//删除所有
+void MainWindow::delAllSlot()
+{
+    int counts=ui->listWidget->count();
+    for(int index=0;index<counts;index++)
+    {
+        QListWidgetItem *item = ui->listWidget->takeItem(ui->listWidget->currentRow());
+        delete item;
+    }
+}
+
+//显示文件
+void MainWindow::showFileSlot()
+{
+    QDir dir=QDir::currentPath();
+    QStringList fileNames=dir.entryList();
+    if(fileNames.size()==0)
+    {
+        QMessageBox::information(this,"error:","have no file here!");
+        return ;
+    }
+    for(int index=0;index<fileNames.size();index++)
+    {
+        if(fileNames.at(index)=="."||fileNames.at(index)=="..")
+            continue;
+        QListWidgetItem *item=new QListWidgetItem;
+        item->setText(fileNames.at(index));
+        ui->listWidget->addItem(item);
+    }
+}
+
+void MainWindow::singleClickedSlot(QListWidgetItem *item)
+{
+    QMessageBox::information(this,"提示:","文件夹"+item->text()+"被单击!");
+}
+
+void MainWindow::doubleClickedSlot(QListWidgetItem *item)
+{
+    QMessageBox::information(this,"提示:","文件夹"+item->text()+"被双击!");
+}
---
+++ mainwindow.h
@@ -0,0 +1,31 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+#pragma execution_character_set("utf-8")
+
+#include <QMainWindow>
+#include<QListWidgetItem>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+    
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    
+private:
+    Ui::MainWindow *ui;
+private slots:
+    void addCitySlot();
+    void delCitySlot();
+    void delAllSlot();
+    void showFileSlot();
+    void singleClickedSlot(QListWidgetItem *);
+    void doubleClickedSlot(QListWidgetItem *);
+};
+
+#endif // MAINWINDOW_H
